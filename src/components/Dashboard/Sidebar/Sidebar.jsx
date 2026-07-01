6f734946cc4b0afc81ddbbcd0ff7a9222e6b3d52
fardin-rahman-sojon <sojoneditz25@gmail.com>
2026-07-01 22:24:43 +0600

    style-increase-sidebar-z-index-to-prevent-overlap

--- src/components/Dashboard/Sidebar/Sidebar.jsx
+++ src/components/Dashboard/Sidebar/Sidebar.jsx
@@ -54,7 +54,7 @@ const Sidebar = () => {
 
       {/* Sidebar */}
       <div
-        className={`z-10 md:fixed flex flex-col justify-between overflow-x-hidden bg-gray-100 w-64 space-y-6 px-2 py-4 absolute inset-y-0 left-0 transform ${
+        className={`z-40 md:fixed flex flex-col justify-between overflow-x-hidden bg-gray-100 w-64 space-y-6 px-2 py-4 absolute inset-y-0 left-0 transform ${
           isActive ? 'translate-x-0' : '-translate-x-full'
         }  md:translate-x-0  transition duration-200 ease-in-out dark:bg-gray-800 dark:text-white`}
       >
